ee48c293e6fc07520094e2bf9703b2f752385004
Mert Kara <mert.kara@b-yond.com>
2018-08-27 22:58:05 -0400

    added the remaining reservation API methods, no service calls yet

--- src/main/java/com/amertkara/campsitemanager/controller/ReservationController.java
+++ src/main/java/com/amertkara/campsitemanager/controller/ReservationController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -25,9 +22,34 @@ public class ReservationController {
 
 	@PutMapping(consumes = APPLICATION_JSON_VALUE)
 	public ResponseEntity<Void> create(@Valid @RequestBody ReservationDTO reservationDTO) {
-		log.debug("Received a create request for a network node payload={}", reservationDTO);
-
-		log.debug("Created the network node networkNodeUuid={}", reservationDTO.getUuid());
+		log.debug("Received a create request for a reservation, payload={}", reservationDTO);
+		// ReservationDTO reservation = reservationService.saveOrUpdate(reservationDTO);
+		log.debug("Created the reservation reservationUuid={}", reservationDTO.getUuid());
+		// return ResponseEntity.created(fromPath(RESERVATIONS_PATH).pathSegment(reservation.getUuid()).build().toUri()).build();
 		return ResponseEntity.created(fromPath(RESERVATIONS_PATH).pathSegment(reservationDTO.getUuid()).build().toUri()).build();
+	}
+
+	@GetMapping(path = "/{reservationUuid}")
+	public ResponseEntity<ReservationDTO> get(@PathVariable String reservationUuid) {
+		log.debug("Received a get request for the reservation reservationUuid={}", reservationUuid);
+		// return ResponseEntity.ok(reservationService.get(reservationUuid));
+		return ResponseEntity.ok(null);
+	}
+
+	@PostMapping(path = "/{reservationUuid}", consumes = APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> update(@PathVariable String reservationUuid, @Valid @RequestBody ReservationDTO reservationDTO) {
+		log.debug("Received an update request for the reservation reservationUuid={}, payload={}", reservationUuid, reservationDTO);
+		// reservationDTO.setUuid(reservationUuid);
+		// reservationService.saveOrUpdate(reservationDTO);
+		log.debug("Updated the reservation reservationUuid={}", reservationUuid);
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping(path = "/{reservationUuid}")
+	public ResponseEntity<Void> delete(@PathVariable String reservationUuid) {
+		log.debug("Received a delete request for the reservation reservationUuid={}", reservationUuid);
+		// reservationService.delete(reservationUuid);
+		log.debug("Deleted the reservation reservationUuid={}", reservationUuid);
+		return ResponseEntity.noContent().build();
 	}
 }
